1756c1703949b78fe36cb6ca330de82f807f904e
unknown <stephaniesimpler@gmail.com>
2020-06-03 19:04:54 -0700

    using discord.utils.find() to find guild

--- bot.py
+++ bot.py
@@ -16,10 +16,14 @@ client = discord.Client()
 @client.event
 #event handler
 async def on_ready():
-    #loop through guilds to find the one you're looking for (only connected to one atm, but bot can connect to multiple)
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    # #loop through guilds to find the one you're looking for (only connected to one atm, but bot can connect to multiple)
+    # for guild in client.guilds:
+    #     if guild.name == GUILD:
+    #         break
+
+    #using discord.utils.find() to loop through guilds (only connected to one atm, but bot can connect to multiple)
+    #"Once find() locates an element in the iterable that satisfies the predicate, it will return the element." - Real Python
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
     print(
         f'{client.user} is connected to the following server:\n'
@@ -28,7 +32,7 @@ async def on_ready():
 
     #print guild members
     members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    # print(f'Guild Members:\n - {members}')
     
 
 client.run(TOKEN)
--- test_bot.py
+++ test_bot.py
@@ -1,3 +1,8 @@
 import unittest
 import bot
 
+class TestBot(unittest.TestCase):
+
+    #"just like any method in a class, this takes self as an argument" - Corey Shafer 
+    def test_on_ready(self):
+        
