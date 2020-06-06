5a9b77478fd76da394107f19e15cdfcacac6bf10
unknown <stephaniesimpler@gmail.com>
2020-06-05 18:38:37 -0700

    Added on_message() so RoboBuddy will respond to messages mentioning them

--- bot.py
+++ bot.py
@@ -21,9 +21,12 @@ async def on_ready():
     #     if guild.name == GUILD:
     #         break
 
-    #using discord.utils.find() to loop through guilds (only connected to one atm, but bot can connect to multiple)
-    #"Once find() locates an element in the iterable that satisfies the predicate, it will return the element." - Real Python
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    # #using discord.utils.find() to loop through guilds (only connected to one atm, but bot can connect to multiple)
+    # #"Once find() locates an element in the iterable that satisfies the predicate, it will return the element... equivalent to break but cleaner" - Real Python
+    # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+    #get takes iterable, keyword arguments, builds predicate and passes to find()
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
         f'{client.user} is connected to the following server:\n'
@@ -33,6 +36,27 @@ async def on_ready():
     #print guild members
     members = '\n - '.join([member.name for member in guild.members])
     # print(f'Guild Members:\n - {members}')
+
+# #untested, need to make test account
+# @client.event
+# async def on_member_join():
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hey {member.name}, welcome to RoboStephSocial!' 
+#         )
+
+#reply to messages about self
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'robobuddy' in message.content.lower():
+        await message.channel.send(
+            f'Hey {message.author} :)'
+            )
+
+
     
 
 client.run(TOKEN)
