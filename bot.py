3b1b149fd2b1aa7b01935af8d134b35cfa14a7b4
unknown <stephaniesimpler@gmail.com>
2020-06-08 15:30:54 -0700

    adding error logging; beginning to convert bot.py to use a bot instead of a client

--- bot.py
+++ bot.py
@@ -6,36 +6,51 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+#client = discord.Client()
 
-@client.event
-#event handler
+#converting our bot.py to use a bot instead of client
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
 async def on_ready():
-    # #loop through guilds to find the one you're looking for (only connected to one atm, but bot can connect to multiple)
-    # for guild in client.guilds:
-    #     if guild.name == GUILD:
-    #         break
-
-    # #using discord.utils.find() to loop through guilds (only connected to one atm, but bot can connect to multiple)
-    # #"Once find() locates an element in the iterable that satisfies the predicate, it will return the element... equivalent to break but cleaner" - Real Python
-    # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-
-    #get takes iterable, keyword arguments, builds predicate and passes to find()
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
 
     print(
-        f'{client.user} is connected to the following server:\n'
+        f'{bot.user.name} has connected to Discord!\n'
         f'{guild.name}(id: {guild.id})'
         )
 
-    #print guild members
-    members = '\n - '.join([member.name for member in guild.members])
-    # print(f'Guild Members:\n - {members}')
+# @client.event
+# #event handler
+# async def on_ready():
+#     # #loop through guilds to find the one you're looking for (only connected to one atm, but bot can connect to multiple)
+#     # for guild in client.guilds:
+#     #     if guild.name == GUILD:
+#     #         break
+
+#     # #using discord.utils.find() to loop through guilds (only connected to one atm, but bot can connect to multiple)
+#     # #"Once find() locates an element in the iterable that satisfies the predicate, it will return the element... equivalent to break but cleaner" - Real Python
+#     # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+#     #get takes iterable, keyword arguments, builds predicate and passes to find()
+#     guild = discord.utils.get(client.guilds, name=GUILD)
+
+#     print(
+#         f'{client.user} is connected to the following server:\n'
+#         f'{guild.name}(id: {guild.id})'
+#         )
+
+#     #print guild members
+#     members = '\n - '.join([member.name for member in guild.members])
+#     # print(f'Guild Members:\n - {members}')
+
+#----------------------------------------------------------------
 
 # #untested, need to make test account
 # @client.event
@@ -45,18 +60,28 @@ async def on_ready():
 #         f'Hey {member.name}, welcome to RoboStephSocial!' 
 #         )
 
-#reply to messages about self
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# #reply to messages about self
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if 'robobuddy' in message.content.lower():
-        await message.channel.send(
-            f'Hey {message.author} :)'
-            )
+#     if 'robobuddy' in message.content.lower():
+#         await message.channel.send(
+#             f'Hey {message.author} :)'
+#             )
 
+# #logging any errors
+# @client.event
+# async def on_error(event, *args, **kwargs):
+#     with open('err.log', 'a') as f: 
+#         if event == 'on_message':
+#             f.write(f'Unhandled message: {args[0]}\n')
+#         else: 
+#             raise
 
     
 
-client.run(TOKEN)
+# client.run(TOKEN)
+
+bot.run(TOKEN)
